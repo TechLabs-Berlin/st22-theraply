781744471cf208a47327c62631130bed6cdcd0e6
0CELINE0 <celine.breuckmann@web.de>
2022-06-08 21:51:02 +0200

    login styling

--- login/login-component/src/LoginForm.js
+++ login/login-component/src/LoginForm.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import './LoginForm.css';
 
 function LoginForm({Login, error}) {
     const [details, setDetails] = useState({name:"", email:"", password:""});
@@ -8,24 +9,27 @@ function LoginForm({Login, error}) {
     };
     return (
         <div className="login-box">
+            <div className="notregistered">Not registered?<a href="#">Create an account</a></div>
         <form onSubmit={submitHandler}>
             <div className='form-inner'>
-                <h2>Login</h2>
-                {(error !="") ? (<div className="error">{error}</div>) :""}
+                <h2 className="h2log">Login</h2><h2 className="h2reg">Registration</h2>
+                {/* <button>Login</button><button>Registration</button> */}
+                {(error !=="") ? (<div className="error">{error}</div>) :""}
             </div>
             <div className='form-group'>
-                <label htmlFor="name">Name: </label>
+                <label htmlFor="name">Name </label>
                 <input type="text" name="name" id="name" onChange={e => setDetails({...details, name: e.target.value})} value={details.name} />
             </div>
             <div className='form-group'>
-                <label htmlFor="email">Email: </label>
+                <label htmlFor="email">Email </label>
                 <input type="email" name="email" id="email" onChange={e => setDetails({...details, email: e.target.value})} value={details.email} />
             </div>
             <div className='form-group'>
-                <label htmlFor="password">Password: </label>
+                <label htmlFor="password">Password </label>
                 <input type="password" name="password" id="password" onChange={e => setDetails({...details, password: e.target.value})} value={details.password} />
             </div>
-            <input type="submit" value="LOGIN" />
+            <input type="submit" value="Login" />
+            <div className="forgotpassword"><a href="#">Forgot Password?</a></div>
         </form>
         </div>
     )
