7e45c9baf4f098d3ee7563707ba4ce34a4e1fa28
adrianschroer <101938708+adrianschroer@users.noreply.github.com>
2022-07-08 16:55:59 +0200

    updating changes

--- backend/index.js
+++ backend/index.js
@@ -1,40 +1,99 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
+const bodyParser = require("body-parser");
 
-const Therapist = require('./models/therapistData.js')
+app.use(express.static(path.join(__dirname, "build")));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/theraplyDB')
-    .then(()=>{
-        console.log('Mongo Connection Open')
-    })
-    .catch(err => {
-        console.log('Mongo connection error')
-        console.log(err)
-    })
+const Therapist = require("./models/therapistData.js");
+const User = require("./models/users.js");
 
+const mongoose = require("mongoose");
+mongoose
+  .connect("mongodb://localhost:27017/theraplyDB")
+  .then(() => {
+    console.log("Mongo Connection Open");
+  })
+  .catch((err) => {
+    console.log("Mongo connection error");
+    console.log(err);
+  });
+// var userExists = false
+//   User.exists({name:'test1'}, function (err, doc) {
+//     if (err){
+//         console.log(err)
+//         console.log(userExists)
+//     }else{
+//         console.log("Result :", doc) 
+//         userExists = true
+//         console.log(userExists)
+//     }
+// });
 
-//homepage
-const homepage = app.use(express.static('../home page'))
-app.get('/', (req, res) => {
-    res.send(homepage)
-})   
+//connection to react
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+  });
 
-//list of therapists
-app.get('/therapists', async (req, res) => {
-    const allTherapists = await Therapist.find({});
-    res.render('allTherapists.ejs', {allTherapists});
-})
-
-app.get('/therapists/:id', async (req, res) => {
-    const {id} = req.params;
-    therapistData = await Therapist.findById(id);
-    console.log(therapistData);
-    res.render('therapistPage.ejs', {therapistData});
-})
-
-app.listen(3000, () => {
-    console.log('listening on port 3000')
+//route for registration requests
+app.post("/registration", function (req, res) {
+  const { newName, newEmail, newPassword } = req.body;
+  if (newName && newEmail && newPassword) {
+    var newUser = new User({
+      name: newName,
+      email: newEmail,
+      password: newPassword,
+    });
+    newUser.save();
+  } else {
+    // res.send("Please fill out all required fields");
+    res.status(400).send('Something broke!')
+  }
 });
 
+//list of therapists
+app.get("/therapists", async (req, res) => {
+  const allTherapists = await Therapist.find({});
+  res.json(allTherapists);
+});
+
+app.get("/therapists/:id", async (req, res) => {
+  const { id } = req.params;
+  therapistData = await Therapist.findById(id);
+  res.json(therapistData);
+});
+
+app.get("/therapists/:id/name", async (req, res) => {
+  const { id } = req.params;
+  therapistData = await Therapist.findById(id);
+  res.json(therapistData.name);
+});
+
+app.get("/therapists/:id/address", async (req, res) => {
+  const { id } = req.params;
+  therapistData = await Therapist.findById(id);
+  res.json(therapistData.address);
+});
+
+app.get("/therapists/:id/website", async (req, res) => {
+  const { id } = req.params;
+  therapistData = await Therapist.findById(id);
+  res.send(therapistData.website);
+});
+
+app.get("/therapists/:id/language", async (req, res) => {
+  const { id } = req.params;
+  therapistData = await Therapist.findById(id);
+  res.json(therapistData.language);
+});
+
+app.get("/therapists/:id/email", async (req, res) => {
+  const { id } = req.params;
+  therapistData = await Therapist.findById(id);
+  res.json(therapistData.email);
+});
+
+app.listen(8800, () => {
+  console.log("listening on port 8800");
+});
