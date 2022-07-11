42bb44bacc8511a432d7d3038217a2c621160e13
vnl2k <38137627+vnl2k@users.noreply.github.com>
2022-07-11 20:17:04 +0200

    cleaned up the folder structure

---
+++ theraply/build/precache-manifest.b2f0e964a2c10618ce69ea390a6a3e65.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ddc431bf15384a70ba46",
+    "url": "/static/css/main.c51905c4.chunk.css"
+  },
+  {
+    "revision": "ddc431bf15384a70ba46",
+    "url": "/static/js/main.8b919e1b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "58e80f5db6f792f8f337",
+    "url": "/static/js/2.009435db.chunk.js"
+  },
+  {
+    "revision": "12d0fc4905551fb8949f19bd771a932d",
+    "url": "/static/media/placeholder-image.12d0fc49.png"
+  },
+  {
+    "revision": "62d6ef7b44a80cabcfae851b94fcd350",
+    "url": "/static/media/Background 2.62d6ef7b.png"
+  },
+  {
+    "revision": "af30e7e5a20aeb66292f6712fe05b5db",
+    "url": "/index.html"
+  }
+];
