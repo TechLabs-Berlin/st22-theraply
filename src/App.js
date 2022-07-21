cc32d048d229ad7abea647cb17c5b0d5cd5c93d0
yellowkobra <97115068+yellowkobra@users.noreply.github.com>
2022-07-21 10:33:09 +0200

    update final version home page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import React from 'react';
-import { Home } from './Home';
-import { Layout } from './Components/Layout';
-import { NavBar } from './Components/NavBar';
+import React from "react";
 
+import { NavBar } from "./Components/NavBar";
+import { Footer } from "./Components/Footer";
+import Home from "./Home";
+import SearchPage from "./SearchPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <React.Fragment>
-      <NavBar />
-      <Layout>
-        <Home />
-      </Layout>
+      <BrowserRouter>
+      
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<SearchPage />} />
+        </Routes>
+
+        <Footer />
+      </BrowserRouter>
     </React.Fragment>
   );
 }
--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,29 @@
 import React from "react";
+import { Layout } from "./Components/Layout";
 
-export const Home = () => {
-    return(
-        <div>
-           
-        </div>
-    );
-};
+import { MiddlePart } from "./Components/MiddlePart";
+import { HomeTopSection } from "./Components/HomeTopSection";
 
-export default Home; 
+import styled from "styled-components";
+import Footer from "./Components/Footer";
+
+const Styles = styled.div`
+  width: 100vw;
+  height: 160vh;
+  background-image: url("/back.jpg");
+  background-size: 100% 100%;
+
+  @media (max-width: 576px) {
+    height: 250vh;
+  }
+`;
+
+export default function Home() {
+  return (
+    <Styles>
+      <HomeTopSection />
+          <MiddlePart />
+          
+    </Styles>
+  );
+}
